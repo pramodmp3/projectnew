1f3cadfbb73703e7d41f5fc1534e280d86323b42
pramodmp3 <pramodmp2308@gmail.com>
2025-10-07 09:51:57 +0530

    Delete navbar.js

--- navbar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("navbar.html")
-    .then((res) => res.text())
-    .then((data) => {
-      document.getElementById("navbar").innerHTML = data;
-    });
-});
